ea872ed74534419b0ef76473aedb1bb4b014717c
yustrovic9 <yustrovic09@gmail.com>
2022-07-07 04:48:25 -0700

    commit0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for index in my_list:
-        print("{:d}".format(index))
+def square_matrix_simple(matrix=[]):
+    return ([list(map(lambda x: x ** 2, i)) for i in matrix])
