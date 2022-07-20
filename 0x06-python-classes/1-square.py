e5c2bd3b9675e10bea9c24203b850d1ed3097435
yustrovic9 <yustrovic09@gmail.com>
2022-07-20 10:58:44 -0700

    commit1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Square module
+"""
+
+
+class Square:
+    """ Define a Square class
+    """
+    def __init__(self, size):
+        """ Intialize methonds that store the size of the square. """
+        self.__size = size
