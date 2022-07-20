1fe6fae9e1bfa6b84ec3dafa91093d40fd992a1e
yustrovic9 <yustrovic09@gmail.com>
2022-07-20 11:49:01 -0700

    commit101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,10 +2,9 @@
 import sys
 
 
-def safe_print_integer_err(value):
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
-        return True
-    except Exception as e:
-        sys.stderr.write("Exception: {}\n".format(e))
-        return False
+        return fct(*args)
+    except (ZeroDivisionError, ValueError, TypeError, IndexError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
