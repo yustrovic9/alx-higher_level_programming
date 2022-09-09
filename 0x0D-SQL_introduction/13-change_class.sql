cdd30543028afcdffe6ef0a58594e2d5ad0ac632
yustrovic9 <yustrovic09@gmail.com>
2022-09-09 06:57:37 -0700

    commit13

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,2 @@
--- removes all records where score is <= 5 from
--- second table
+-- removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0
 DELETE FROM second_table WHERE score <= 5;
